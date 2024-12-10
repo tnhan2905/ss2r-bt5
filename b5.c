1317aa007fdff0163d35ecf57c34a9794629554e
tnhan2905 <diepthanhnhan2019.st@gmail.com>
2024-12-10 23:50:18 +0700

    Add files via upload

---
+++ b5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int lenght = 5, width = 10;
+    int perimeter = (lenght + width) * 2, area = lenght * width;
+    printf("perimeter of rectangle = %d\n", perimeter);
+    printf("area of rectangle = %d", area);
+    
+    return 0;
+}
